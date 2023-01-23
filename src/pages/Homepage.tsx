3b7d722ec4c560304fc49e4066c638ffa4e466c6
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-01-23 15:15:23 +0100

    [feat] eslint, typescript-eslint and prettier config

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom'
 
 const Homepage = () => (
   <div className="w-screen h-screen bg-indigo-500 flex items-center justify-center">
-    <Link to="/museum" className="rounded-md p-4 bg-white shadow-md hover:shadow-lg transition-shadow"><span className="font-sans">Visit the museum</span></Link>
+    <Link
+      to="/museum"
+      className="rounded-md p-4 bg-white shadow-md hover:shadow-lg transition-shadow"
+    >
+      <span className="font-sans">Visit the museum</span>
+    </Link>
   </div>
 )
 
