f3cbad46302d3f1129d5be1ed15a23f4c8d6d220
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-01-23 14:28:48 +0100

    initial tailwind setup + routing

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+
+const Homepage = () => <div>
+  <strong className="block">homepage</strong>
+  <Link to="/museum">Visit the museum</Link>
+</div>
+
+export default Homepage
---
+++ src/pages/Museum.tsx
@@ -0,0 +1,3 @@
+const Museum = () => <div>museum</div>
+
+export default Museum
