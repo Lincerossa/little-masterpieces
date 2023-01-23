417c21e310be147604e6da989f56188949938ac3
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-01-23 20:59:06 +0100

    feat: minor stylistic things

--- src/pages/Museum.tsx
+++ src/pages/Museum.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Stars } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 
 import Floor from '../components/Floor'
@@ -7,11 +7,20 @@ import Room from '../components/Room'
 import { CEILING_HEIGHT, GROUND_FLOOR } from '../utils/consts'
 
 const Museum = () => (
-  <div className="w-screen h-screen bg-indigo-500">
+  <div className="w-screen h-screen bg-indigo-900">
     <Canvas>
       <OrbitControls />
       <ambientLight intensity={2} />
-      <pointLight position={[10, CEILING_HEIGHT + 5, 1]} color="#ea580c" intensity={1} />
+      <Stars
+        radius={100}
+        depth={40}
+        count={5000}
+        factor={10}
+        saturation={10}
+        fade
+        speed={1}
+      />
+      <pointLight position={[0, CEILING_HEIGHT, -10]} color="#312e81" intensity={10} />
       <Floor position={[0, GROUND_FLOOR, 0]} />
       <Room
         position={[0, GROUND_FLOOR + CEILING_HEIGHT / 2, 0]}
