04b27afa669b8f1c56cb0b1ac7ffda27b960ace2
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-01-25 15:53:39 +0100

    fix: removed duplicated images because of duplicated keys

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -3,12 +3,6 @@ export const CEILING_HEIGHT = 2
 
 export const IMAGES = [
   'https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHBhaW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1533158388470-9a56699990c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1055&q=80',
-  'https://images.unsplash.com/photo-1607457561901-e6ec3a6d16cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-  'https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHBhaW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1533158388470-9a56699990c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1055&q=80',
-  'https://images.unsplash.com/photo-1607457561901-e6ec3a6d16cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-  'https://images.unsplash.com/photo-1543857778-c4a1a3e0b2eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHBhaW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1533158388470-9a56699990c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1055&q=80',
+  'https://images.unsplash.com/photo-1533158388470-9a56699990c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
   'https://images.unsplash.com/photo-1607457561901-e6ec3a6d16cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
 ]
