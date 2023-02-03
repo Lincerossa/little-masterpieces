c82d324621a6d5ae9473d3da9c2cd46c52e7708e
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-02-04 00:01:21 +0100

    feat: lights controlled by leva

--- src/components/Lights.tsx
+++ src/components/Lights.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable react/no-unknown-property */
+import { useControls } from 'leva'
 import { memo } from 'react'
 
 import random from '../utils/helpers/random'
 const Lights = () => {
+  const { lightsColor, mainLightColor, lightsQuantity } = useControls({
+    lightsColor: '#a10de5',
+    mainLightColor: '#312e81',
+    lightsQuantity: { value: 10, min: 5, max: 20 },
+  })
   return (
     <>
       <ambientLight intensity={2} />
-      <pointLight position={[0, 50, -10]} color="#312e81" intensity={10} />
-      {Array.from({ length: 20 }).map((e, i) => (
+      <pointLight position={[0, 50, -10]} color={mainLightColor} intensity={20} />
+      {Array.from({ length: lightsQuantity }).map((e, i) => (
         <pointLight
           key={i}
           intensity={0.2}
-          color={i % 2 ? '#312e81' : '#ea580c'}
-          position={[random(-200, 200), random(10, 30), random(-50, -100)]}
+          color={lightsColor}
+          position={[random(-100, 100), random(10, 30), random(-50, -50)]}
         />
       ))}
     </>
