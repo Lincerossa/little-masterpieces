183e33a02694fb2d6554e80824087ffacfc875c5
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-02-04 00:00:37 +0100

    feat: textureLoader added to the floar.. should be a nice Murble floor

--- src/components/Floor.tsx
+++ src/components/Floor.tsx
@@ -1,16 +1,36 @@
 /* eslint-disable react/no-unknown-property */
-import { Vector3 } from '@react-three/fiber'
+import { useLoader, Vector3 } from '@react-three/fiber'
 import { FC } from 'react'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 type FloorProps = {
   position: Vector3
 }
 
 const Floor: FC<FloorProps> = ({ position }) => {
+
+
+  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(
+    TextureLoader,
+    [
+      'Marble016_1K_Color.jpg',
+      'Marble016_1K_Displacement.jpg',
+      'Marble016_1K_NormalDX.jpg',
+      'Marble016_1K_NormalGL.jpg',
+      'Marble016_1K_Roughness.jpg',
+    ],
+  )
+
   return (
     <mesh position={position} rotation={[-Math.PI / 2, 0, 0]}>
       <planeGeometry args={[500, 500]} />
-      <meshBasicMaterial attach="material" color="#84319B" />
+      <meshStandardMaterial
+        map={colorMap}
+        displacementMap={displacementMap}
+        normalMap={normalMap}
+        roughnessMap={roughnessMap}
+        aoMap={aoMap}
+      />
     </mesh>
   )
 }
