83f44b80d47504dd1f76d5fee812a495701feb7c
marcelloluatti <marcello.luatti@cloudacademy.com>
2023-02-04 11:37:41 +0100

    feat: camera flyBy enabled

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,24 +1,29 @@
+import { OrbitControls } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import { FC } from 'react'
+import { useControls } from 'leva'
+import { FC,  useState } from 'react'
 import { MathUtils } from 'three'
 
 import { MeshGeometryBaseProps } from '../utils/types'
 
 type CameraProps = {
-  cameraPosition: MeshGeometryBaseProps['position']
-  setCameraPosition: (e: null) => void
+  position: MeshGeometryBaseProps['position']
 }
 
 const SPEED = 0.05
 const DISTANCE = 4
 const DEVIATION = 0.2
 
-const Camera: FC<CameraProps> = ({ setCameraPosition, cameraPosition }) => {
-  const [X, Y, Z] = cameraPosition
+const Camera: FC<CameraProps> = ({ position }) => {
+  const [orbit, showOrbit] = useState(false)
+  const { flyByEnabled } = useControls({
+    flyByEnabled: true,
+  })
+  const [X, Y, Z] = position
 
   useFrame(({ camera }) => {
     const { x, y, z } = camera.position
-
+    if (!flyByEnabled) return
     camera.position.x = MathUtils.lerp(x, X, SPEED)
     camera.position.y = MathUtils.lerp(y, Y, SPEED)
     camera.position.z = MathUtils.lerp(z, Z + DISTANCE, SPEED)
@@ -29,10 +34,10 @@ const Camera: FC<CameraProps> = ({ setCameraPosition, cameraPosition }) => {
       Math.abs(y - Y) < DEVIATION &&
       Math.abs(z - Z) < DEVIATION + DISTANCE
     ) {
-      setCameraPosition(null)
+      showOrbit(true)
     }
   })
-  return null
+  return orbit ? <OrbitControls /> : null
 }
 
 export default Camera
